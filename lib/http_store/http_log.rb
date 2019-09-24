c10634141e09f2687d51544bba67dd23a6678fa3
black <black@paimingjia.com>
2019-09-24 19:00:47 +0800

    fix: parent optional

--- lib/http_store/http_log.rb
+++ lib/http_store/http_log.rb
@@ -2,7 +2,7 @@ module HttpStore
   class HttpLog < ActiveRecord::Base
     belongs_to :requestable, polymorphic: true, optional: true
 
-    has_many :sons, class_name: 'HttpStore::HttpLog', foreign_key: 'parent_id', optional: true
+    has_many :sons, class_name: 'HttpStore::HttpLog', foreign_key: 'parent_id'
     belongs_to :parent, class_name: 'HttpStore::HttpLog', optional: true
   end
 end
