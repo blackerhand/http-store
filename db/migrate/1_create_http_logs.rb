6ba68532c986f6e88dba9b1bbda16cd1b8eaadc3
black <black@paimingjia.com>
2023-12-15 11:08:02 +0800

    add table index

--- db/migrate/1_create_http_logs.rb
+++ db/migrate/1_create_http_logs.rb
@@ -1,17 +1,17 @@
 class CreateHttpLogs < ActiveRecord::Migration[5.2]
   def change
-    create_table :http_logs do |t|
+    create_table "http_logs_#{i}" do |t|
       # request
-      t.string :http_method, comment: '请求方法'
+      t.string :http_method, limit: 10, comment: '请求方法'
       t.text :url, comment: '请求 URL'
-      t.string :data_type, comment: 'data 参数类型 (form/json)'
+      t.string :data_type, limit: 10, comment: 'data 参数类型 (form/json)'
       t.text :headers, comment: '请求 header'
       t.text :query_params, comment: 'url 参数'
       t.text :data, comment: 'body, get/delete 为空'
 
       # request builder
       t.text :other_params, comment: '其他自定义参数, 用于构建 query_params/data_type'
-      t.string :request_digest, comment: '请求唯一 id'
+      t.string :request_digest, limit: 80, comment: '请求唯一 id'
       t.boolean :request_valid, comment: '请求是否符合规范, 若为 false, 不会发起请求'
       t.boolean :force, comment: '忽略缓存, 强制请求'
 
@@ -25,10 +25,14 @@ class CreateHttpLogs < ActiveRecord::Migration[5.2]
       t.text :response_data, comment: '格式化后的 response'
 
       # relation
-      t.string :client_type, comment: '请求类型'
+      t.string :client_type, limit: 80, comment: '请求类型'
       t.string :requestable_id, comment: '外键 ID'
       t.string :requestable_type, comment: '外键 类型'
       t.integer :parent_id
+
+      t.index [:created_at, :client_type]
+      t.index [:created_at, :response_valid]
+      t.index :request_digest
 
       t.timestamps
     end
