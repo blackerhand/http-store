43473672af33fdeb49668365bcc8268f00edc201
black <black@paimingjia.com>
2024-08-13 17:25:38 +0800

    modify job bug

--- lib/http_store/job/http_log_store_job.rb
+++ lib/http_store/job/http_log_store_job.rb
@@ -4,7 +4,7 @@ module HttpStore
       queue_as :default
 
       def perform(meta)
-        @storeable_record = HttpStore.config.store_class.new(meta)
+        @storeable_record = HttpStore.config.store_class.to_s.constantize.new(meta)
         @storeable_record.save!
       end
     end
